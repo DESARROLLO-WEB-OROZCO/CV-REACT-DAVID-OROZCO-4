aca293797b5bc25d4f68b70a0b250fbdf90fdebc
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-11-23 00:42:57 -0500

    chore: organización inicial del proyecto para integración de eventos

---
+++ src/cvData.js
@@ -0,0 +1,8 @@
+export const tecnologiasIniciales = ["React", "JavaScript", "HTML", "CSS"];
+
+export const habilidadesIniciales = [
+  "Comunicación",
+  "Trabajo en equipo",
+  "Liderazgo",
+  "Pensamiento lógico",
+];
