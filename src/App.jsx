873921a0401034aa711a8ee26152d5da76c22531
david alejandro orozco henao <davidalejandroorozcohenao@gmail.com>
2025-11-23 00:50:48 -0500

    feat: función agregarTecnologia en App.jsx y paso como prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import ToggleHabilidades from "./components/ToggleHabilidades";
 import Habilidades from "./components/Habilidades";
+import FormularioTecnologia from "./components/FormularioTecnologia";
 
 function App() {
   const [mostrarHabilidades, setMostrarHabilidades] = useState(true);
 
+  const [tecnologias, setTecnologias] = useState([
+    "React",
+    "JavaScript",
+    "HTML",
+    "CSS"
+  ]);
+
+  const agregarTecnologia = (tec) => {
+    setTecnologias([...tecnologias, tec]);
+  };
+
   return (
     <div>
-      <ToggleHabilidades
-        mostrarHabilidades={mostrarHabilidades}
-        setMostrarHabilidades={setMostrarHabilidades}
-      />
+      <ToggleHabilidades mostrar={mostrarHabilidades} setMostrar={setMostrarHabilidades} />
 
-      {mostrarHabilidades && <Habilidades />}
+      {mostrarHabilidades && <Habilidades tecnologias={tecnologias} />}
+
+      <FormularioTecnologia agregarTecnologia={agregarTecnologia} />
     </div>
   );
 }
